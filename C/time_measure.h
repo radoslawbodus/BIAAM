b4a6b4de3f1edbb58e0e2f1828726fd1f3234fbd
radoslawbodus <radoslaw.bodus1@wp.pl>
2023-03-30 23:10:07 +0200

    Further cleaning

--- C/time_measure.h
+++
@@ -1,7 +0,0 @@
-// Repeat the function "iterations" times and report the average and total time
-// Both in CPU time and Wall time
-void repeat_func(void (*f)(int), int iterations, int argc, ...);
-
-// Dummy test function
-void count_to_n(int n);
-
